4694a3ecde3d64bf208d07b5639715b682505dee
DevEssien <essienemma300@gmail.com>
2024-04-11 09:38:50 +0100

    added decorator patterns

---
+++ design_patterns/behavioral_pattern/decorator.js
@@ -0,0 +1,32 @@
+class Car {
+	constructor() {
+		this.price = 2000000;
+	}
+
+	getPrice() {
+		return this.price;
+	}
+}
+
+class CarWithOptions {
+	constructor(car) {
+		this.car = car;
+	}
+
+	addGPS() {
+		return (this.car.price += 800);
+	}
+
+	addRim() {
+		return (this.car.price += 500);
+	}
+}
+
+const car = new Car();
+console.log("car ", car.getPrice());
+
+const carWithOptions = new CarWithOptions(car);
+carWithOptions.addGPS();
+carWithOptions.addRim();
+
+console.log("car with options ", carWithOptions.car.getPrice());
